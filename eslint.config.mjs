81078cc0c8055ac7e6881b447eb92f5448cd0be2
TheSpanishBread <61657019+TheSpanishBread@users.noreply.github.com>
2025-08-22 11:15:40 +0800

    Initial Commit

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettierPlugin from "eslint-plugin-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    plugins: { js, prettier: prettierPlugin },
+    rules: {
+      ...js.configs.recommended.rules, // ESLint recommended rules
+      "prettier/prettier": "error", // Prettier issues as ESLint errors
+    },
+  },
+]);
